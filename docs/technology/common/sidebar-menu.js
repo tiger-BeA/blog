1d5b9e4b8a19b70ae18b37e455f96eff5899fd19
tiger-BeA <>
2021-12-02 11:55:09 +0000

    Deploying to gh-pages from @ tiger-BeA/blog@0daf003e74955013c945ce538489a50ec250a7ca 🚀

--- docs/technology/common/sidebar-menu.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  title: '通用',
-  path: '/technology/common/',
-  collapsable: false,
-  children: [
-    '/technology/common/clean-code'
-  ]
-}
--- docs/technology/server/sidebar-menu.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  title: '后端',
-  path: '/technology/server/',
-  collapsable: false,
-  children: [
-    '/technology/server/share-mySQL',
-    '/technology/server/concurrency'
-  ]
-}
